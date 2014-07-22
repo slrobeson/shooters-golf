aa592e06f9c29437d5473032ed7513d5f5dd619a
jnathanking <jnathanking@gmail.com>
2014-07-22 14:47:02 -0400

    More models.

--- ShootersGolf/Models/Player.cs
+++ ShootersGolf/Models/Player.cs
@@ -1,7 +1,32 @@
-﻿namespace ShootersGolf.Models
+﻿using System;
+using System.ComponentModel;
+
+namespace ShootersGolf.Models
 {
     public class Player
     {
-         
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime RegisteredDateTime { get; set; }
+        public ShirtSize ShirtSize { get; set; }
+        public string TeamId { get; set; }
+    }
+
+    public enum ShirtSize
+    {
+        [Description("S")]
+        S = 1,
+        [Description("M")]
+        M = 2,
+        [Description("L")]
+        L = 3,
+        [Description("XL")]
+        XL = 4,
+        [Description("2XL")]
+        XXL = 5,
+        [Description("3XL")]
+        XXXL = 6
     }
 }
---
+++ ShootersGolf/Models/Sponsor.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace ShootersGolf.Models
+{
+    public class Sponsor
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public SponsorLevel Level { get; set; }
+    }
+
+    public enum SponsorLevel
+    {
+        [Description("Hole - $100")]
+        Hole = 1,
+        [Description("T-Shirt - $100")]
+        TShirt = 2,
+        [Description("Hole In One - $150")]
+        HoleInOne = 3
+    }
+}
