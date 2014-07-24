131999edac9d48d73720b77fcd26758c7a491e47
slrobeson <slrobeson@gmail.com>
2014-07-24 09:16:01 -0400

    fix steves screwup

---
+++ ShootersGolf/Controllers/PlayersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Raven.Client;
+using ShootersGolf.Models;
+
+namespace ShootersGolf.Controllers
+{
+    public class PlayersController : ApiController
+    {
+        [HttpGet]
+        public IList<Player> Get()
+        {
+            using (var session = Global.DocumentStore.OpenSession())
+            {
+                return session.Query<Player>().Take(int.MaxValue).ToList();
+            }
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Post(Player player)
+        {
+            player.RegisteredDateTime = DateTime.Now;
+
+            using (var session = Global.DocumentStore.OpenSession())
+            {
+                session.Store(player);
+                session.SaveChanges();
+
+                return new HttpResponseMessage(HttpStatusCode.Created);
+            }
+        }
+    }
+}
---
+++ ShootersGolf/app/adminControllers.js
@@ -0,0 +1,37 @@
+﻿angular.module("adminApp")
+    .controller("playersCtrl", function ($scope, $http) {
+
+        $scope.players = [];
+
+        $http.get("http://localhost:20534/api/players")
+            .success(function (data) {
+                $scope.players = data;
+            })
+            .error(function (error) {
+                $scope.data.error = error;
+            });
+    })
+    .controller("teamsCtrl", function ($scope, $http) {
+
+        $scope.teams = [];
+
+        $http.get("http://localhost:20534/api/teams")
+            .success(function (data) {
+                $scope.teams = data;
+            })
+            .error(function (error) {
+                $scope.data.error = error;
+            });
+    })
+    .controller("sponsorsCtrl", function ($scope, $http) {
+
+        $scope.sponsors = [];
+
+        $http.get("http://localhost:20534/api/sponsors")
+            .success(function (data) {
+                $scope.sponsors = data;
+            })
+            .error(function (error) {
+                $scope.data.error = error;
+            });
+    });
